159f4cda9906fe64022adc9d4867d57c2e38faf6
chernyak88 <i_chernyak@inbox.ru>
2019-09-22 16:22:39 +0300

    add feedback and adaptive

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+///////////////// Слайдер
+
 var slides = document.querySelectorAll('.slider__list .slider__item');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 4000);
@@ -40,3 +42,86 @@ var previous = document.getElementById('prev');
       pauseSlideshow();
       previousSlide();
   };
+
+  ///////////////// Обратная связь
+
+  const feedbackBtn = document.querySelector('.contacts__feedback');
+  const feedbackOverlay = document.querySelector('.feedbackoverlay');
+  const feedback = document.querySelector('.feedback');
+  const closeBtn = document.querySelector('.feedback__close');
+
+  feedbackBtn.addEventListener("click", function() {
+    feedbackOverlay.classList.add('visible');
+    feedback.classList.add('visible');
+    document.body.classList.add('hidden');
+  })
+
+  closeBtn.addEventListener('click', function() {
+    feedbackOverlay.classList.remove('visible');
+    feedback.classList.remove('visible');
+    document.body.classList.remove('hidden');
+  });
+
+  feedbackOverlay.addEventListener('click', function() {
+    event.preventDefault();
+    feedbackOverlay.classList.remove('visible');
+    feedback.classList.remove('visible');
+    document.body.classList.remove('hidden');
+  })
+
+  feedback.addEventListener('click', function(e) {
+    e.stopImmediatePropagation();
+  })
+
+  ///////////////// Валидация формы
+
+  const form = document.querySelector('.form');
+  const formBtn = document.querySelector('.form__btn');
+  const name = document.getElementById('name');
+  const phone = document.getElementById('phone');
+  const email = document.getElementById('email');
+
+  function validate() {
+    if (name.value == "") {
+        name.classList.add("error");
+        name.setAttribute('placeholder', 'Поле обязательно для заполнения');
+        if (name.value !== "") {
+          name.classList.remove("error");
+        }
+        return false;
+    }
+    if (phone.value == "") {
+        phone.classList.add("error");
+        phone.setAttribute('placeholder', 'Поле обязательно для заполнения');
+      if (phone.value !== "") {
+        phone.classList.remove("error");
+      }
+      return false;
+    }
+    if (email.value == "") {
+      email.classList.add("error");
+      email.setAttribute('placeholder', 'Поле обязательно для заполнения');
+    if (email.value !== "") {
+      email.classList.remove("error");
+    }
+  return false;
+}
+    else {
+      return true;
+    } 
+  }
+
+  name.addEventListener("focus", function() {
+    name.classList.remove("error");
+    name.setAttribute('placeholder', '');
+  })
+
+  phone.addEventListener("focus", function() {
+    phone.classList.remove("error");
+    phone.setAttribute('placeholder', '');
+  })
+
+  email.addEventListener("focus", function() {
+    email.classList.remove("error");
+    email.setAttribute('placeholder', '');
+  })
